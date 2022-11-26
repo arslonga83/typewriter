1a58992a45aa3b9cf87124cd057606cd4b30039e
arslonga83 <jlwood83@gmail.com>
2022-11-26 15:40:30 -0800

    add setinterval to the function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const container = document.querySelector('.container')
+let index = 0; 
 
 const text = [
   'Hello, Friend...', 
-  'Welcome to my home.'
+  'Welcome to my home.',
+  'You are just in time for the party!'
 ]
 
 document.addEventListener('keyup', (e) => {
   if (e.code === 'Space') {
-    printNewText();
+    setInterval(() => {
+      printNewText();
+    }, 3000)
+    
   }
 })
 
 function printNewText() {
-  for (let phrase of text) {
-      container.innerHTML = `<p class='typed'>${phrase}</p>`   
-  }
+      if (index < text.length) {
+        container.innerHTML = `<p class='typed'>${text[index]}</p>`   
+      index++
+      }
+      
+  
 }
